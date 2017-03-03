cee3e8068b14a239dbe78b7234099fa0a1811108
Ralle1968 <graef.ralf@gmail.com>
2017-03-03 22:13:08 +0100

    Finishing task

--- src/ArraySumme.java
+++ src/ArraySumme.java
@@ -2,33 +2,21 @@
 public class ArraySumme {
 
 	public static void main(String[] args) {
-		/*
-		 * 4 2 0
-		 * 9 5 2
-		 * 1 0 0
-		 */
+		
 		int[][] arr = new int[][] {{4,2},{9,5,2},{1}};
 		
-		int[] sum = new int[] {0};
+		
 		int summe = 0;
 		
-//		System.out.println(arr.length);
-//		
-//		System.out.println(arr[0].length);
-//		System.out.println(arr[1].length);
-//		System.out.println(arr[2].length);
-//		
-//		System.out.println(arr[0][0]);
-//		System.out.println(arr[0][1]);
-		
+
 		for (int x = 0; x < arr.length; x++) {
 			for (int y = 0; y < arr[x].length; y++) {
-				System.out.println(arr[x][y]);
+				summe += (arr[x][y]);
 			}
 			
 		}
 	
-		
+		System.out.println(summe);
 	}
 
 }
